d6b674de99a3a455b5cf609f5d1976ab1b8fd579
Sean Lin <linx0486@umn.edu>
2015-12-10 00:10:42 -0600

    add readme

--- AustinSean/tictactoe/PlayTTT.py
+++ AustinSean/tictactoe/PlayTTT.py
@@ -15,24 +15,30 @@ class TTTgui:
         self.buttons = [[None for i in range(0,3)] for j in range(0,3)]
         self.depth = depth
 
+
     def play(self):
+
+        # Set up tkinter window
         print("Setting up game...\n")
         self.game = TicTacToe()
         self.player2 = AdversarialSearch(self.game.copy(),"O", self.depth)
         window = Tk()
         window.rowconfigure((0,3), weight=1)
         window.columnconfigure((0,2), weight=1)
+
+        # Setup grid buttons
         for i in range(0,3):
             for j in range(0,3):
                 b = Button(window, text = "", pady = 2, width = 5, height = 5, command = lambda a=i,b=j,: self.takeTurn(a,b))
                 b.grid(row = i, column = j)
                 self.buttons[i][j] = b
 
-
+        # setup reset button
         buttonR = Button(window, text = "RESET", width = 15, height = 5, command =self.reset)
         buttonR.grid(row = 3, column = 0, columnspan = 3)
         window.mainloop()
 
+    # resets the game
     def reset(self):
         self.game = TicTacToe()
         alpha = -1
@@ -42,7 +48,9 @@ class TTTgui:
             for j in range(0,3):
                 self.buttons[i][j]["text"] = ""
 
+    # makes player move, then ai move
     def takeTurn(self, y, x):
+        # execute player's move
         if self.game.setPiece(x,y,"X") == -1:
             return
         self.buttons[y][x]["text"] = "X"
@@ -56,7 +64,7 @@ class TTTgui:
                 print(status + " WINS")
             return
 
-
+        # execute ai move
         self.player2.state = self.game.copy()
         (t,(x2,y2)) = self.player2.minValue(self.player2.state)
         self.game.setPiece(x2,y2,"O")
@@ -77,6 +85,7 @@ class TTTgame:
         self.depth = depth
 
     def play(self):
+        # setup board and players
         print("Setting up game...\n")
         game = TicTacToe()
 
@@ -84,6 +93,7 @@ class TTTgame:
         player2 = AdversarialSearch(game.copy(),"O", self.depth)
         while game.isGameOver() == " ":
 
+            # player 1's turn
             print("Player 1 is deciding\n")
 
             player1.state = game.copy()
@@ -94,6 +104,7 @@ class TTTgame:
             if game.isGameOver() != " ":
                 break
 
+            # player 2's turn
             print("Player 2 is deciding\n")
 
             player2.state = game.copy()
@@ -113,6 +124,7 @@ class TTTABgame:
         self.temp = 0
 
     def play(self):
+        # setup board and players
         print("Setting up game...\n")
         game = TicTacToe()
         alpha = -1
@@ -120,7 +132,7 @@ class TTTABgame:
         player1 = AlphaBetaSearch(game.copy(),"X",alpha,beta)
         player2 = AlphaBetaSearch(game.copy(),"O",alpha,beta)
         while game.isGameOver() == " ":
-
+            # player 1's turn
             print("Player 1 is deciding\n")
 
             player1.state = game.copy()
@@ -130,7 +142,7 @@ class TTTABgame:
             print(game)
             if game.isGameOver() != " ":
                 break
-
+            # player 2's turn
             print("Player 2 is deciding\n")
 
             player2.state = game.copy()
@@ -145,6 +157,8 @@ class TTTABgame:
         else:
             print(status + " WINS")
 
+
+# setup functions that run the games
 def runGUI(depth):
     gui = TTTgui(depth)
     gui.play()
@@ -156,14 +170,14 @@ def runGame(depth):
     t1 = time()
     print("Time elapsed: ",t1-t0)
 
-def runABGame(depth):
-    game = TTTABgame(depth)
+def runABGame():
+    game = TTTABgame()
     t0 = time()
     game.play()
     t1 = time()
     print("Time elapsed: ",t1-t0)
 
-
+# menu setup
 depth = dict()
 title = menu.TextDisplay("Enter the number of the choice to choose it. Enter 'quit' to quit.")
 gameChoice = menu.ChoicePrompt("Which game do you want to run?",
@@ -177,15 +191,13 @@ titleNode = menu.menuNode(title, [], None)
 root = menu.menuNode(gameChoice, [], None)
 guiDepth = menu.menuNode(depthInput, [], root)
 gameDepth = menu.menuNode(depthInput, [], root)
-ABDepth = menu.menuNode(depthInput, [], root)
 guiNode = menu.menuNode(guiRun, [], root)
 gameNode = menu.menuNode(gameRun, [], root)
 ABNode = menu.menuNode(ABRun, [], root)
 
 titleNode.setChildren([root])
-root.setChildren([guiDepth, gameDepth, ABDepth])
+root.setChildren([guiDepth, gameDepth, ABNode])
 guiDepth.setChildren([guiNode])
 gameDepth.setChildren([gameNode])
-ABDepth.setChildren([ABNode])
 
 titleNode.execute()
---
+++ AustinSean/tictactoe/README.txt
@@ -0,0 +1,9 @@
+HOW TO RUN CODE:
+Run the python file PlayTTT.py in the shell.
+Follow the instructions that appear.
+
+You can either play against the AI (which uses Adversarial Search),
+or have the AI play against another AI. The search depth can be set
+for Adversarial Search. It cannot be set for Alpha Beta Search. When
+playing against the AI, a new game window will open.
+All the code is complete
